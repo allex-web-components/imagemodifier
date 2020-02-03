338c354c9f3b5e242a96e7235a868e2034b8c41d
Andrija Petrovic <andrija.hers@gmail.com>
2020-02-03 07:20:42 +0000

    edit mode

--- index.js
+++ index.js
@@ -17,13 +17,30 @@
     this.cropper = null;
     ImgElement.prototype.__cleanUp.call(this);
   };
-  ImageModifierElement.prototype.onLoaded = function (evnt) {
-    ImgElement.prototype.onLoaded.call(this, evnt);
+  ImageModifierElement.prototype.onImgLoaded = function (evnt) {
+    this.set('edit', false);
     this.$element.css('display', 'block').css('max-width', '100%');
+    ImgElement.prototype.onImgLoaded.call(this, evnt);
+  };
+  ImageModifierElement.prototype.onImgError = function (evnt) {
+    this.set('edit', false);
+    ImgElement.prototype.onImgError.call(this, evnt);
+  };
+  ImageModifierElement.prototype.get_edit = function () {
+    return !!this.cropper;
+  };
+  ImageModifierElement.prototype.set_edit = function (edit) {
     if (this.cropper) {
       this.cropper.destroy();
     }
-    this.cropper = new Cropper(this.$element[0], this.getConfigVal('cropper')||{});
+    this.cropper = null;
+    if (!edit) {
+      return true;
+    }
+    if (!this.cropper) {
+      this.cropper = new Cropper(this.$element[0], this.getConfigVal('cropper')||{});
+    }
+    return true;
   };
 
   applib.registerElementType ('ImageModifierElement', ImageModifierElement);
